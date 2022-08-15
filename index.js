a9ace476b73345d2a45a33470eeb1a12190ab581
Javier Miranda <mirandavillegasjavier@gmail.com>
2022-08-15 13:37:48 -0400

    Primer Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import router from './routes/index.js';
+import db from './config/db.js'
+
+const app = express();
+
+// Contectar la base de datos
+db.authenticate()
+    .then(() => console.log('Base de datos conectada'))
+    .catch(() => console.log('error'))
+
+
+// Definir puerto
+const port  = process.env.PORT || 4000;
+
+// Habilitar pug 
+app.set('view engine', 'pug');
+
+// Obtener el año actual
+app.use((req, res, next) => {
+    const year= new Date();
+
+    res.locals.actualYear = year.getFullYear();
+    res.locals.nombreSitio = "Agencia de Viajes";
+
+    next()
+});
+
+// Agregar body parser para leer los datos del formulario
+app.use(express.urlencoded({extended: true}));
+
+// Definir la carpeta publica
+app.use(express.static('public'));
+
+// Agregar router
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log(`El servidor está funcionando en el puerto ${port}`)
+});
